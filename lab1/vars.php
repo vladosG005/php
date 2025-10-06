9b41050239c5519f4cf4755878e4070747ec80fb
vladosG005 <vladosG005@yandex.ru>
2025-10-06 13:44:52 +0300

    Do lab1 task 5

--- lab1/vars.php
+++ lab1/vars.php
@@ -1,30 +1,23 @@
 <?php
-/*
-ЗАДАНИЕ 1
-- Создайте переменную $name и присвойте ей значение содержащее Ваше имя, например 'Иван'(обязательно в одинарных кавычках!).
-- Создайте переменную $age и присвойте ей значение содержащее Ваш возраст, например 20.
-*/
+ $name = 'Владислав';
+ $age = 20;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Переменные и вывод</title>
-</head>
-<body>
-	<h1>Переменные и вывод</h1>
-	<?php
-	/*
-	ЗАДАНИЕ 2
-	- Выведите с помощью echo фразу "Меня зовут: $name", например: 'Меня зовут: Иван'.
-	- Выведите фразу "Мне $age лет", например: 'Мне 20 лет'.
-	- Выведите информацию о типе переменных $name и $age.
-	- Удалите переменные $name и $age.
-	- Измените код так, чтобы каждая фраза начиналась с новой строки.
-	- Изолируйте код PHP от HTML-разметки.
-	*/
-	?> 
-</body>
+ <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Переменные и вывод</title>
+ </head>
+ <body>
+  <h1>Переменные и вывод</h1>
+  <?php
+   echo "Меня зовут: {$name}", PHP_EOL;
+   echo "Мне {$age} лет", PHP_EOL;
+   echo gettype($name), PHP_EOL;
+   echo gettype($age), PHP_EOL;
+   unset($name, $age);
+  ?> 
+ </body>
 </html>
