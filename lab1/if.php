00614dcc624753b95d8e2e543f37912b6b75891f
vladosG005 <vladosG005@yandex.ru>
2025-10-24 15:41:05 +0300

    Create if.php: do task 2

---
+++ lab1/if.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Конструкции if-elseif-else</title>
+ </head>
+ <body>
+  <h1>Конструкции if-elseif-else</h1>
+
+  <?php
+
+  $age = 20;
+  
+  if (18 <= $age && $age <= 59) {
+      echo "Вам ещё работать и работать", PHP_EOL;
+  }
+  elseif ($age > 59) {
+      echo "Вам пора на пенсию", PHP_EOL;
+  }
+  elseif (1 <= $age && $age <= 17) {
+      echo "Вам ещё рано работать", PHP_EOL;
+  }
+  else {
+      echo "Неизвестный возраст", PHP_EOL;
+  }
+
+  ?>
+
+ </body>
+</html>
--- lab1/vars.php
+++ lab1/vars.php
@@ -9,15 +9,21 @@
  <body>
   <h1>Переменные и вывод</h1>
   <?php
+
    $name = 'Владислав';
    $age = 20;
    echo '<pre>';
+
    echo "Меня зовут {$name}", PHP_EOL;
    echo "Мне {$age} лет", PHP_EOL;
+
    echo 'Тип переменной $name — ', gettype($name), PHP_EOL;
    echo 'Тип переменной $age — ', gettype($age), PHP_EOL;
+
    unset($name, $age);
+
    echo '</pre>';
+   
   ?> 
  </body>
 </html>
