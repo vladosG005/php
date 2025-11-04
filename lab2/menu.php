a5e7333a38e956c1e3719a6e03ddf7abe67d8098
vladosG005 <vladosG005@yandex.ru>
2025-11-04 17:01:45 +0300

    Create lab2/menu.php

---
+++ lab2/menu.php
@@ -0,0 +1,39 @@
+<?php
+ declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Меню</title>
+  <style>
+   .menu {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+   }
+  </style>
+ </head>
+ <body>
+
+  <h1>Меню</h1>
+  <nav>
+   <?php
+    $leftMenu = [['link' => 'Домой', 'href' => 'index.php'],
+                 ['link' => 'О нас', 'href' => 'about.php'],
+                 ['link' => 'Контакты', 'href' => 'contact.php'],
+                 ['link' => 'Таблица умножения', 'href' => 'table.php'],
+                 ['link' => 'Калькулятор', 'href' => 'calc.php'],];
+    
+    echo '<ul class="menu">';
+    foreach($leftMenu as $navelement) {
+        echo '<li><a href="', $navelement['href'], '">',
+             $navelement['link'], '</a></li>';
+    }
+    echo '</ul>';
+   ?> 
+  </nav>
+ </body>
+</html>
