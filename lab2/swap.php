ad08843c0d0a011ad2a0aac8262e36f0af09d33f
vladosG005 <vladosG005@yandex.ru>
2025-11-04 17:22:57 +0300

    Create lab2/swap.php

---
+++ lab2/swap.php
@@ -0,0 +1,30 @@
+<?php
+ declare(strict_types=1);
+
+ /**
+  * Меняет местами значения двух переменных.
+  *
+  * @param mixed $x Первая переменная.
+  * @param mixed $y Вторая переменная.
+  *
+  * @return void
+  */
+ $swap = function(mixed &$x, mixed &$y): void {
+     $t = $x;
+     $x = $y;
+     $y = $t;
+ };
+
+ $a = 5;
+ $b = 8;
+
+ $swap($a, $b);
+
+ echo '<pre>';
+ if ($b === 5) {
+     echo 'b равно 5.', PHP_EOL;
+ }
+ if ($a === 8) {
+     echo 'a равно 8.', PHP_EOL;
+ }
+ echo '</pre>';
