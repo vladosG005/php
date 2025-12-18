99d7e3228d426fdaaea581970cbb42ccb004748b
vladosG005 <vladosG005@yandex.ru>
2025-12-18 17:52:55 +0300

    Create SuperUser class

--- lab7/src/Classes/SuperUser.php
+++ lab7/src/Classes/SuperUser.php
@@ -3,5 +3,15 @@
 
  require_once 'User.php';
 
+//PHPDoc
  class SuperUser extends User {
+     public function __construct(string $name, string $login, string $password,
+     public string $role) {
+         parent::__construct($name, $login, $password);
+     }
+
+     public function showInfo(): void {
+         echo '<pre>Имя: ', $this->name, PHP_EOL, 'Логин: ', $this->login,
+         PHP_EOL, 'Роль: ', $this->role, '</pre>';
+     }
  }
--- lab7/users.php
+++ lab7/users.php
@@ -1,7 +1,8 @@
 <?php
  declare(strict_types=1);
 
- include 'src/Classes/User.php';
+ require_once 'src/Classes/User.php';
+ require_once 'src/Classes/SuperUser.php';
  
  $user1 = new User('Павел', 'koren12', 'DZL8pr');
  $user1->showInfo();
@@ -12,3 +13,6 @@
  $user3 = new User('Саня', 'burdraco', 'CIUzgLp');
  $user3->showInfo();
  
+ $user = new SuperUser('Алексей', 'root',
+ 'N1Y1GXYgKYs7mB1T', 'Администратор');
+ $user->showInfo();
