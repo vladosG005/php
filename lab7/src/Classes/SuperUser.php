3eba79581e3ad3c76152927a0ed9078545fa63ad
vladosG005 <vladosG005@yandex.ru>
2026-01-13 15:53:24 +0300

    Add PHPDoc

--- lab7/src/Classes/SuperUser.php
+++ lab7/src/Classes/SuperUser.php
@@ -4,13 +4,26 @@
  namespace App\Classes;
  require_once 'User.php';
 
- //PHPDoc
+ /**
+  * Класс, в котором хранятся имя, логин и роль суперпользователя.
+  */
  class SuperUser extends User {
+     /**
+      * Конструктор, принимающий имя, логин, пароль и роль суперпользователя.
+      *
+      * @param string $name Имя суперпользователя.
+      * @param string $login Логин суперпользователя.
+      * @param string $password Пароль суперпользователя.
+      * @param string $role Роль суперпользователя.
+      */
      public function __construct(string $name, string $login, string $password,
      public string $role) {
          parent::__construct($name, $login, $password);
      }
 
+     /**
+      * Выводит имя, логин и роль суперпользователя.
+      */
      public function showInfo(): void {
          echo '<pre>Имя: ', $this->name, PHP_EOL, 'Логин: ', $this->login,
          PHP_EOL, 'Роль: ', $this->role, '</pre>';
--- lab7/src/Classes/User.php
+++ lab7/src/Classes/User.php
@@ -3,11 +3,23 @@
 
  namespace App\Classes;
 
- //PHPDoc
+ /**
+  * Класс, в котором хранятся имя, логин и пароль пользователя.
+  */
  class User {
+     /**
+      * Конструктор, принимающий имя, логин, пароль пользователя.
+      *
+      * @param string $name Имя пользователя.
+      * @param string $login Логин пользователя.
+      * @param string $password Пароль пользователя.
+      */
      public function __construct(public string $name, public string $login,
      private string $password) {}
 
+     /**
+      * Выводит имя, логин и пароль пользователя.
+      */
      public function showInfo(): void {
          echo '<pre>Имя: ', $this->name, PHP_EOL, 'Логин: ', $this->login,
          PHP_EOL, 'Пароль: ', $this->password, PHP_EOL, '</pre>';
